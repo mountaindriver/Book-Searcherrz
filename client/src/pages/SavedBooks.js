974e242c9c960310454f01d253741ffd177d3e50
Mountian Driver <lsfreigenberg@outlook.com>
2022-11-12 10:52:16 -0700

    Create new component BookList

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,49 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { Jumbotron, Container } from 'react-bootstrap';
+import BookList from '../components/BookList';
 
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
+// Apollo code 
+import { useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState({});  
+  // Apollo code
+  const { loading, data} = useQuery(GET_ME);
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
-
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        // checks if user is logged in
-        const token = 
-        // checks if user is logged in
-        Auth.loggedIn() 
-        // retrieves token from local storage
-        ? Auth.getToken() 
-        : null;
-
-        if (!token) {
-          return false;
-        }
-
-        // asks for the token?? i think
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    // What is the purpose of this?
-    getUserData();
-  }, [userDataLength]);
-
+  if (data){
+    setUserData(data);
+  }
+  
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -68,11 +43,6 @@ const SavedBooks = () => {
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
-
   return (
     <>
       <Jumbotron fluid className='text-light bg-dark'>
@@ -82,11 +52,16 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
+          {loading ? (
+            <div>Loading...</div>
+          ):(
+            userData.savedBooks.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
+            : 'You have no saved books!'
+          )}
         </h2>
-        <CardColumns>
+        <BookList books={userData}/>
+        {/* <CardColumns>
           {userData.savedBooks.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
@@ -102,7 +77,7 @@ const SavedBooks = () => {
               </Card>
             );
           })}
-        </CardColumns>
+        </CardColumns> */}
       </Container>
     </>
   );
